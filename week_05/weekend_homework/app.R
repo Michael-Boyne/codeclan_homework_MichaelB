aaa1c0127e88e1272ebb2d32efc827eaad17f78d
Michael-Boyne <72604230+Michael-Boyne@users.noreply.github.com>
2020-11-23 09:16:43 +0000

    homework_final_submit

--- week_05/weekend_homework/app.R
+++ week_05/weekend_homework/app.R
@@ -45,28 +45,42 @@ ui <- fluidPage(
 # the selected constituency.
        
                   tabPanel("Graph",
-                
-                      plotOutput("election_results")
+                           
+                           plotOutput(outputId = "election_results")
+                           
                   ),
                   tabPanel("Table",
-                
-                      dataTableOutput("vote")
-                  ),
+                           
+                           dataTableOutput(outputId = "vote")
+                  )
               )
            )
         )
     ),
+
+# A main tab that allows you to switch to the full results throughout Scotland
+# is useful to gain some perspective on the wider results, which can be 
+# a useful comparison.
+
     tabPanel("Full Results",
              
+             tabsetPanel(
+               tabPanel("Graph", 
              
-      
+                      plotOutput(outputId = "full_results")
+               ),
+               
+               tabPanel("data",
+             
+                      dataTableOutput(outputId = "total_vote")
+               )
+             )
     )
+  )
+)
 
 
 server <- function(input, output) { 
-  
-  
-
   
   output$election_results <- renderPlot({
     
@@ -84,21 +98,46 @@ server <- function(input, output) {
           "lab_votes" = "#ff471a",
           "brex_votes" = "#00cccc",
           "ukip_votes" = "#9933ff",
-          "oth_votes" = "#8c8c8c"
-        )
-      )
+          "oth_votes" = "#8c8c8c"))
     
   })
   
-  output$percentage <- 
-  
   output$vote <- renderDataTable({
+    
     voting_results()
+
   })
   
-  
-  
-}
+  output$full_results <- renderPlot({
+   
+    voting_results %>%
+      group_by(party) %>%
+      summarise(total_count = sum(vote_count)) %>%
+      ggplot() +
+      aes(x = party, y = vote_count, fill = party) +
+      geom_col() +
+      scale_fill_manual(
+        values = c(
+          "snp_votes" = "#ffff33",
+          "con_votes" = "#3333ff",
+          "ld_votes" = "#ff9900",
+          "grn_votes" = "#00b33c",
+          "lab_votes" = "#ff471a",
+          "brex_votes" = "#00cccc",
+          "ukip_votes" = "#9933ff",
+          "oth_votes" = "#8c8c8c"
+        ))
 
+  })
+  
+  output$total_vote <- renderDataTable({
+    
+    voting_results() %>%
+      group_by(party) %>%
+      summarise(total_count = sum(vote_count))
+      
+  })
+
+}
 
 shinyApp(ui = ui, server = server)
